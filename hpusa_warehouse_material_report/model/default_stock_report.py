b96ce91023ed5479e1029054c5cb86a483d48d51
longnguyen1803 <longnguyen180392@gmail.com>
2016-03-22 17:00:01 +0700

    Thay đổi sql trong function tính hao hut  trong warehouse

--- hpusa_warehouse_material_report/model/default_stock_report.py
+++ hpusa_warehouse_material_report/model/default_stock_report.py
@@ -1685,31 +1685,29 @@ class inventory_report(osv.osv):
             ((round(coalesce(sum(tab6.weight_gr),0),3)
             -round(coalesce(sum(tab6.weight_gr)/sum(tab6.weight_gr)*sum(diamond.weight_gr),0),3))) as loss_weight,
             pp.coeff_24k as coeff_24k,
+            wk.percent as percent,
+            mp.metal_in_product as net_weight,
             round(coalesce(sum(tab1.qty),0),3)
             + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) )
             - ( round(coalesce(sum(tab2.qty),0),3) + (round(coalesce(sum(tab6.weight_gr),0),3)- round(coalesce(sum(tab3.weight_gr),0),3)+  round(coalesce(sum(tab4.weight_gr),0),3)))
             as loss,
-            (round(round(coalesce(sum(tab1.qty),0),3)
-            + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) ) ))* wk.percent /100
+            mp.metal_in_product * wk.percent /100
             as loss_limit,
             round(coalesce(sum(tab1.qty),0),3)
             + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) )
             - ( round(coalesce(sum(tab2.qty),0),3) + (round(coalesce(sum(tab6.weight_gr),0),3)- round(coalesce(sum(tab3.weight_gr),0),3)+  round(coalesce(sum(tab4.weight_gr),0),3)))
-             -  ((round(round(coalesce(sum(tab1.qty),0),3)
-            + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) ) ))* wk.percent /100)
+             -  ( mp.metal_in_product * wk.percent /100)
              as loss_over,
            ( round(coalesce(sum(tab1.qty),0),3)
             + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) )
             - ( round(coalesce(sum(tab2.qty),0),3) + (round(coalesce(sum(tab6.weight_gr),0),3)- round(coalesce(sum(tab3.weight_gr),0),3)+  round(coalesce(sum(tab4.weight_gr),0),3))))
             * coeff_24k as loss_24k,
-              ((round(round(coalesce(sum(tab1.qty),0),3)
-            + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) ) ))* wk.percent /100)
+               ( mp.metal_in_product* wk.percent /100)
             *coeff_24k as loss_limit_24k,
              (round(coalesce(sum(tab1.qty),0),3)
             + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) )
             - ( round(coalesce(sum(tab2.qty),0),3) + (round(coalesce(sum(tab6.weight_gr),0),3)- round(coalesce(sum(tab3.weight_gr),0),3)+  round(coalesce(sum(tab4.weight_gr),0),3)))
-             -  ((round(round(coalesce(sum(tab1.qty),0),3)
-            + (round(coalesce(sum(tab5.weight_gr),0),3) -  round(coalesce(sum(tab3.weight_gr),0),3) + round(coalesce(sum(tab4.weight_gr),0),3) ) ))* wk.percent /100))
+             -  ( mp.metal_in_product* wk.percent /100))
             *coeff_24k as loss_over_24k
             from mrp_production_workcenter_line mpwl
              left join mrp_workcenter as wk on(wk.id = mpwl.workcenter_id)
@@ -2013,7 +2011,7 @@ class inventory_report(osv.osv):
                 ) as mrp
                 GROUP BY mrp.id
                 ) as diamond ON(diamond.id = mpwl.production_id)
-                group by mp.name,mpwl.date_planned, mpwl.name,pp.coeff_24k,wk.percent
+                group by mp.name,mpwl.date_planned, mpwl.name,pp.coeff_24k,wk.percent, mp.metal_in_product
                 having coalesce(sum(tab1.qty),0)
                 +coalesce(sum(tab1.qty_24k),0)+ coalesce(sum(tab2.qty),0)
                 +coalesce(sum(tab2.qty_24k),0)+ coalesce(sum(tab3.weight_ct),0)
